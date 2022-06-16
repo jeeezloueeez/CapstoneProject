7ce317b50f6fef65372b6c82f89bbc3ad0e0886f
jeeezloueeez <velazquez117@gmail.com>
2022-06-16 16:24:46 -0500

    Successfully sent a request for a specific product in the Home Depot API and returned image, title and price of product and appended each item into my DOM

--- public/main.js
+++ public/main.js
@@ -1,20 +1,48 @@
 const productID = document.getElementById('product-id')
 const form = document.getElementById('search-form')
+const itemResults = document.querySelector('.item-results')
+const saveButton = document.getElementById('save-button')
+
+const url = 'http://localhost:5011'
 
 const searchID = (evt) => {
   evt.preventDefault()
   let idNumber = productID.value
   axios
-    .get(`http://localhost:5011/product/${idNumber}`)
+    .get(`${url}/product/${idNumber}`)
     .then(res => {
-      console.log(res.data)
+      const { product_results } = res.data
+      const { images, title, price } = product_results
+      let newSection = document.createElement('div')
+      newSection.classList.add('item')
+      itemResults.appendChild(newSection)
+      let image = document.createElement('img')
+      image.src = images[0]
+      newSection.appendChild(image)
+      let productName = document.createElement('h3')
+      productName.textContent = title
+      newSection.appendChild(productName)
+      let cost = document.createElement('h4')
+      cost.textContent = `$${price}`
+      newSection.appendChild(cost)
+      saveButton.style.visibility = 'visible'
     })
     .catch(err => {console.log(err)})
   productID.value = ''
 }
 
+const saveList = (evt) => {
+  evt.preventDefault()
+
+  axios
+    .post()
+    .then()
+    .catch(err => {console.log(err)})
+}
+
 
 form.addEventListener('submit', searchID)
+saveButton.addEventListener('click', saveList)
 
 
 
--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,17 @@
+require('dotenv').config()
+const apiKey = process.env.API_KEY
+const axios = require('axios')
+
 module.exports = {
   getID: (req, res) => {
-    let productID
+    let productInfo
     const { id } = req.params
     axios
-      .get(``)
-      .then(res => {
-        productID = res.data
+      .get(`https://serpapi.com/search.json?engine=home_depot_product&product_id=${id}&api_key=${apiKey}`)
+      .then(response => {
+        productInfo = response.data
+        res.status(200).send(productInfo)
       })
       .catch(err => {console.log(err)})
-    res.status(200).send(productID)
   },
 }
