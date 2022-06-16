b0b67f931514558e47f722f65ffc60ce737f84a3
jeeezloueeez <velazquez117@gmail.com>
2022-06-16 17:08:15 -0500

    Heroku Fixes pt.2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+const path = require('path')
 let express = require('express');
 let cors = require('cors');
 const axios = require('axios')
@@ -7,13 +8,17 @@ const dotEnv = require('dotenv-webpack')
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'))
 
 const controllerFile = require('./controller')
 
 
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 app.get('/product/:id', controllerFile.getID) 
 
 
