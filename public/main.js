683815ca96c64fbc96c9e085ad2b76fa73aea1f9
jeeezloueeez <velazquez117@gmail.com>
2022-06-21 08:33:09 -0500

    Heroku Fixes

--- public/main.js
+++ public/main.js
@@ -9,15 +9,16 @@ const searchID = (evt) => {
   evt.preventDefault()
   let idNumber = productID.value
   axios
-    .get(`${url}/product/${idNumber}`)
+    .get(`/product/${idNumber}`)
     .then(res => {
       const { product_results } = res.data
       const { images, title, price } = product_results
+      console.log(images)
       let newSection = document.createElement('div')
       newSection.classList.add('item')
       itemResults.appendChild(newSection)
       let image = document.createElement('img')
-      image.src = images[0]
+      image.src = images[0][0]
       newSection.appendChild(image)
       let productName = document.createElement('h3')
       productName.textContent = title
